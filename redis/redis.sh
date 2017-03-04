cd6ba86067d6742316101678aa91257d36ccfc8a
Alexei Zhuravlev <serangu@yandex.ru>
2017-03-04 19:32:42 +0300

    Доработка конфигурации БД

--- redis/redis.sh
+++ redis/redis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/ridemate/redis/redis_6441/src/redis-server /usr/local/ridemate/redis/conf/redis_6441.conf
+/usr/local/ridemate/redis/redis/src/redis-server /usr/local/ridemate/redis/conf/redis_6441.conf
 
 sleep 1
 echo 'redis_6441 ok'
