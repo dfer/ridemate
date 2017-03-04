6afa31041a44d9400be2e801e8fe48641be9d249
Alexei Zhuravlev <serangu@yandex.ru>
2017-03-04 18:33:16 +0300

    Уточение параметров подключения к БД

--- redis/redis.sh
+++ redis/redis.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-/usr/lib/ruby/1.8/cosmoslot/redis/redis_6441/src/redis-server /usr/lib/ruby/1.8/cosmoslot/redis/conf/redis_6441.conf
+/usr/local/ridemate/redis/redis_6441/src/redis-server /usr/local/ridemate/redis/conf/redis_6441.conf
 
 sleep 1
 echo 'redis_6441 ok'
-
-/usr/lib/ruby/1.8/cosmoslot/redis/redis_6442/src/redis-server /usr/lib/ruby/1.8/cosmoslot/redis/conf/redis_6442.conf
-
-sleep 1
-echo 'redis_6442 ok'
-
-/usr/lib/ruby/1.8/cosmoslot/redis/redis_6443/src/redis-server /usr/lib/ruby/1.8/cosmoslot/redis/conf/redis_6443.conf
-
-sleep 1
-echo 'redis_6443 ok'
-
-/usr/lib/ruby/1.8/cosmoslot/redis/redis_6444/src/redis-server /usr/lib/ruby/1.8/cosmoslot/redis/conf/redis_6444.conf
-
-sleep 1
-echo 'redis_6444 ok'
