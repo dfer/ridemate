44f494dbd28b464fc4f84316c595c9ed951cfb4e
Alexei Zhuravlev <serangu@yandex.ru>
2017-03-04 20:56:22 +0300

    Смотрим что присылают Одноклассники чтобы взять фотографию.

--- lib/oauth.rb
+++ lib/oauth.rb
@@ -42,6 +42,9 @@ get '/ok_action' do
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 		request = Net::HTTP::Post.new(uri.request_uri)
 		response = http.request(request)
+		
+		write_log(response.body)
+		
 		# Получаем {"access_token": "kjdhfldjfhgldsjhfglkdjfg9ds8fg0sdf8gsd8fg", "token_type": "session", "refresh_token": "klsdjhf0e9dyfasduhfpasdfasdfjaspdkfjp"}
 		hash = JSON.parse response.body
 		
